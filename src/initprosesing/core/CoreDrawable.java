750f2374c327e2eedc88ff35a69db61ca0dc1b74
IsekaiRid <mainrpg62@gmail.com>
2026-03-28 14:39:54 +0700

    level 1.e

--- src/initprosesing/core/CoreDrawable.java
+++ src/initprosesing/core/CoreDrawable.java
@@ -1,13 +1,14 @@
 package initprosesing.core;
 
 import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Fill;
 import arc.scene.style.Drawable;
 
 public class CoreDrawable implements Drawable {
 
-    private float radius;
-    private Color color;
+    float radius;
+    Color color;
 
     public CoreDrawable(float radius, Color color){
         this.radius = radius;
@@ -16,12 +17,19 @@ public class CoreDrawable implements Drawable {
 
     @Override
     public void draw(float x, float y, float width, float height){
-        Fill.color(color);
-        Fill.roundRect(x, y, width, height, radius);
-        Fill.color(); // reset
+
+        Draw.color(color);
+
+        Fill.rect(
+            x + width/2f,
+            y + height/2f,
+            width,
+            height
+        );
+
+        Draw.color();
     }
 
-    // optional (biar layout ngerti ukuran)
     @Override public float getMinWidth(){ return 0; }
     @Override public float getMinHeight(){ return 0; }
 }
