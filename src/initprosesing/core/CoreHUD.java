158c3de3315623f9028fd68f90d89c2a3566e604
IsekaiRid <mainrpg62@gmail.com>
2026-03-28 12:15:43 +0700

    level 1.4.1

--- src/initprosesing/core/CoreHUD.java
+++ src/initprosesing/core/CoreHUD.java
@@ -1,30 +1,31 @@
 package initprosesing.core;
 
+import arc.Events;
 import arc.scene.ui.layout.*;
 import mindustry.Vars;
+import mindustry.game.EventType.*;
 
 public class CoreHUD {
 
     public static void init(){
 
-        // ambil HUD group
-        Table table = new Table();
+        Events.on(ClientLoadEvent.class, e -> {
 
-        // posisi pojok kanan atas
-        table.top().right();
-        table.margin(10);
+            Table table = new Table();
 
-        // isi UI
-        table.add("[lightgray]==== Info Mod ==== []").row();
+            table.top().right();
+            table.margin(10);
 
-        table.add("[accent]Init Prosesing Core[]")
-            .padTop(5)
-            .row();
+            table.add("[lightgray]==== Info Mod ==== []").row();
 
-        table.add("Status: [green]Active[]")
-            .padTop(5);
+            table.add("[accent]Init Prosesing Core[]")
+                .padTop(5)
+                .row();
 
-        // tambahkan ke HUD
-        Vars.ui.hudGroup.addChild(table);
+            table.add("Status: [green]Active[]")
+                .padTop(5);
+
+            Vars.ui.hudGroup.addChild(table);
+        });
     }
 }
