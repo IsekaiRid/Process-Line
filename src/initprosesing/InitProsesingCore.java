f257c4831288759ce5f96427cd8dcfc4a2a208dc
IsekaiRid <mainrpg62@gmail.com>
2026-03-28 11:22:31 +0700

    level 1

---
+++ src/initprosesing/InitProsesingCore.java
@@ -0,0 +1,15 @@
+package initprosesing;
+
+import mindustry.mod.*;
+
+public class InitProsesingCore extends Mod {
+
+    public InitProsesingCore(){
+        System.out.println("InitProsesingCore Loaded!");
+    }
+
+    @Override
+    public void loadContent(){
+        System.out.println("Loading InitProsesingCore content...");
+    }
+}
